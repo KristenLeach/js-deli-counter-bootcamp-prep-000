45f8e56373fecdaaa4e7d756db03373380fb4b94
Kristen Leach <kristenleach24@gmail.com>
2017-11-24 19:09:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ function takeANumber(katzDeliLine, newName){
   katzDeliLine.push(newName);
   return`Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`
 }
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length > 0){
+  var firstPerson = katzDeliLine.splice(0,1);
+  return firstPerson;
+  }
+  else {
+    return "There is nobody waiting to be served!";
+  }
+}
