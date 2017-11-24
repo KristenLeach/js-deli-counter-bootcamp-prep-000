f8a71078bdcbb37a8b6ed1e2e7272b8d4f334d7f
Kristen Leach <kristenleach24@gmail.com>
2017-11-24 19:05:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, newName){
   katzDeliLine.push(newName);
-  return`Welcome ${newName}. You are number ${katzDeliLine.length} in line.`
+  return`Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`
 }
